4e4a8d2332d097e13664ac96bb3a861fed5bfd83
Chris Anders <github@hell-labs.de>
2013-06-06 23:25:18 +0200

    Prototypical implementation of some methods

--- day4/sinatra/coffee_machine_web/cm_sinatra.rb
+++ day4/sinatra/coffee_machine_web/cm_sinatra.rb
@@ -8,6 +8,44 @@ class WebCoffeeMachine < Sinatra::Base
   get '/' do
     erb :layout
   end
+
+  get '/info' do
+    settings.cm.info
+  end
+
+  get '/fill' do
+    settings.cm.refill_water(1000)
+    settings.cm.refill_coffee(100)
+    "filled"
+  end
+  
+  get '/refill_coffee/:id' do
+    amount = params[:id]
+    begin
+      settings.cm.refill_coffee(amount.to_i)
+    rescue ToMuchCoffeeException
+      "Zuviel Kaffee eingefuellt, bitte Fuellmenge reduzieren."
+    end
+  end
+
+  get '/refill_water/:id' do
+    amount = params[:id]
+    begin
+      settings.cm.refill_water(amount.to_i)
+    rescue ToMuchWaterException
+      "Zuviel Wasser eingefuellt, bitte Fuellmenge reduzieren." 
+    end
+  end
+
+  get '/make' do
+    begin
+      settings.cm.make_coffee
+    rescue NoCoffeeException
+      "Bitte Kaffee auffuellen."
+    rescue NoWaterException
+      "Bitte Wasser auffuellen."
+    end
+  end
 end
 
 #settings parameter
